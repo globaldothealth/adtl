542ea67e9bed0b36a472f91c73ff8c2b79e15ca7
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2023-02-22 10:39:18 +0000

    Add tests for transformations.py

---
+++ tests/test_transformations.py
@@ -0,0 +1,28 @@
+import sys
+import json
+from pathlib import Path
+import pytest
+
+import adtl as parser
+import adtl.transformations as transform
+
+
+@pytest.mark.parametrize(
+    "test_input,expected", [("1", True), (None, False), ("", False)]
+)
+def test_isNotNull(test_input, expected):
+    assert transform.isNotNull(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "test_date_birth, test_date_current, expected",
+    [
+        ("1996-02-22", "2023-02-22", 27.0),
+        ("", "2023-02-22", None),
+        (None, "2023-02-22", None),
+    ],
+)
+def test_yearsElasped(test_date_birth, test_date_current, expected):
+    assert transform.yearsElapsed(test_date_birth, test_date_current) == pytest.approx(
+        expected, 0.001
+    )
