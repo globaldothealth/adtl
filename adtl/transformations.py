0caa556522dcbc59f8feeaf372d2f5e7354173d8
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2023-03-09 12:38:32 +0000

    Apply funcs + tests for duration of observation. One returns days elapsed, the other an ISO time period as y-m-d(start)/y-m-d(end)

--- adtl/transformations.py
+++ adtl/transformations.py
@@ -17,3 +17,32 @@ def yearsElapsed(birthdate, currentdate):
 
     days = cd - bd
     return pint.Quantity(days.days, "days").to("years").m
+
+
+def durationPeriod(symptomstartdate, currentdate):
+    """Returns an ISO time interval (start-Date/end-Date)."""
+    if symptomstartdate in [None, ""] or currentdate in [None, ""]:
+        return None
+
+    sd = datetime.strptime(symptomstartdate, "%Y-%m-%d")
+    sd = sd.strftime("%Y-%m-%d")
+    cd = datetime.strptime(currentdate, "%Y-%m-%d")
+    cd = cd.strftime("%Y-%m-%d")
+
+    return sd + "/" + cd
+
+
+def durationDays(symptomstartdate, currentdate):
+    """
+    Returns the number of days between two dates.
+    Preferable to Y-M-D elapsed, as month length is ambiguous -
+    can be anywhere between 28-31 days.
+    """
+    if symptomstartdate in [None, ""] or currentdate in [None, ""]:
+        return None
+
+    bd = datetime.strptime(symptomstartdate, "%Y-%m-%d")
+    cd = datetime.strptime(currentdate, "%Y-%m-%d")
+
+    days = cd - bd
+    return days.days
