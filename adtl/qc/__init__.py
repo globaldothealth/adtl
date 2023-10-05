ca01aa65e914b576ca49465e3c59abb1f0f9bec6
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2023-10-05 16:02:36 +0100

    qc: add support for rule collection

--- adtl/qc/__init__.py
+++ adtl/qc/__init__.py
@@ -1,13 +1,29 @@
 """
 Quality Control module for ADTL
 """
+import sys
+import argparse
 import functools
 import importlib
 from typing import List, Union
 from pathlib import Path
+from typing import TypedDict
 
 import numpy as np
 import pandas as pd
+
+DEFAULT_PATTERN = "*.csv"
+
+
+class Rule(TypedDict):
+    rule: str
+    description: str
+    pattern: str
+
+
+def rules_for(pattern: str, *rules):
+    for r in rules:
+        r.pattern = pattern
 
 
 def rule(columns: List[str], mostly: float = 0):
@@ -36,6 +52,8 @@ def rule(columns: List[str], mostly: float = 0):
                     rows_fail=rows_fail,
                     ratio_success=ratio_success,
                     success=ratio_success >= mostly,
+                    mostly=mostly,
+                    series=series,
                 )
             elif isinstance(series, bool):
                 return dict(
@@ -54,14 +72,41 @@ def schema(schema_path: Union[str, Path], pattern: str = "*.csv"):
     pass
 
 
-def collect_rules(root: Path = Path("qc")):
+def collect_rules(root: Path = Path("qc")) -> list[Rule]:
     rule_files = list(root.glob("*.py"))
-    print(rule_files)
+
+    sys.path.append(".")
+    module_name = lambda path: str(path).replace(".py", "").replace("/", ".")
+    for rule_file in rule_files:
+        module = importlib.import_module(module_name(rule_file))
+        rules = [x for x in dir(module) if x.startswith("rule_")]
+        print(module_name(rule_file), ":", [_rule_properties(module, r) for r in rules])
 
 
-def main():
-    print("ADTL Quality Control Module")
-    print(collect_rules())
+def _rule_properties(module, rule_name: str) -> Rule:
+    r = getattr(module, rule_name)
+    return dict(
+        rule=r.__name__,
+        description=r.__doc__,
+        pattern=getattr(r, "pattern", DEFAULT_PATTERN),
+    )
+
+
+def collect_datasets(root: Path = Path("."), file_format="csv") -> list[Path]:
+    data = root.rglob(f"*.{file_format}")
+    print(list(data))
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser(prog="adtl-qc", description="ADTL Quality Control")
+    parser.add_argument("data", help="path to datasets")
+    parser.add_argument("-r", "--rule-root", help="path to rules", default=".")
+    parser.add_argument(
+        "--format", help="file format to include in datasets", default="csv"
+    )
+    args = parser.parse_args(args)
+    collect_rules(Path(args.rule_root))
+    collect_datasets(Path(args.data), args.format)
 
 
 if __name__ == "__main__":
