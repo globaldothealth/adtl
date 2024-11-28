79805871d49b279e95edce6eec38576ca62c9f55
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2024-11-28 11:42:46 +0000

    Add tests

---
+++ tests/test_python_interface.py
@@ -0,0 +1,13 @@
+import adtl
+from pathlib import Path
+
+
+def test_parse(snapshot):
+    adtl.parse(
+        "tests/parsers/epoch.json",
+        "tests/sources/epoch.csv",
+        output="output",
+        encoding="utf-8",
+    )
+    assert Path("output-table.csv").read_text() == snapshot
+    Path("output-table.csv").unlink()
