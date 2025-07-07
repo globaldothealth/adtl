11de6aa8a2b01d3e94e23d43878a988b258f179e
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2025-07-07 11:09:41 +0100

    Migrate '_relabel_choices()'

--- src/adtl/autoparser/mapping.py
+++ src/adtl/autoparser/mapping.py
@@ -93,14 +93,18 @@ class BaseMapper(abc.ABC):
 
         self.data_dictionary = format_dict(data_dictionary, config=self.config)
 
-    @abc.abstractmethod
-    def create_mapping(self, save=True, file_name="mapping_file") -> pd.DataFrame:
-        pass
+    # Abstract methods ---------------------------------------
 
     @abc.abstractmethod
     def _iter_value_tuples(self) -> list[tuple[str, list[str], list[str]]]:
         "Yield (field_name, source_values, target_values) triples for the LLM."
         pass
+
+    @abc.abstractmethod
+    def create_mapping(self, save=True, file_name="mapping_file") -> pd.DataFrame:
+        pass
+
+    # Common properties ---------------------------------------
 
     @cached_property
     def common_values(self) -> pd.Series:
@@ -152,35 +156,56 @@ class BaseMapper(abc.ABC):
     def mapped_fields(self, value: pd.Series):
         self._mapped_fields = value
 
-    def match_values_to_schema(self) -> pd.DataFrame:
+    def _relabel_choices(self, map_df):
         """
-        Use the LLM to match the common values from the data dictionary to the target
-        values in the schema - i.e. enum or boolean options.
+        If 'choices' are present in the data dictionary, relabel the choices
+        in the mapping dataframe to match the source field names.
+        e.g.
+        if the data dictionary indicates {1: Man, 2:Female, 3:Unknown}, and the LLM maps
+        {Man:male, Female:female, Unknown:unknown}, then `relabel_choices` should give
+        {1:male, 2:female, 3:unknown}
         """
 
-        values_tuples = list(self._iter_value_tuples())
+        filtered_dict = self.data_dictionary[
+            self.data_dictionary["source_field"].isin(map_df["source_field"])
+        ][["source_field", "choices"]]
 
-        # to LLM
-        value_pairs = self.model.map_values(values_tuples, self.language)
+        mapping_choices = map_df[["source_field", "value_mapping"]]
 
-        value_mapping = {}
+        choices = filtered_dict.merge(mapping_choices, on="source_field")
+        choices["combined_choices"] = choices.apply(
+            lambda x: (
+                {k: x["value_mapping"].get(v) for k, v in x["choices"].items()}
+                if isinstance(x["choices"], dict)
+                else None
+            ),
+            axis=1,
+        )
 
-        for p in value_pairs.values:
-            f = p.field_name
-            value_dict = {
-                pair.source_value: pair.target_value for pair in p.mapped_values
-            }
-            value_mapping[f] = value_dict
+        merged_mapping_dict = (
+            map_df.reset_index()
+            .merge(
+                choices[["source_field", "combined_choices"]],
+                on="source_field",
+                how="left",
+            )
+            .drop_duplicates(subset="target_field")
+            .set_index("target_field")
+        )
+        mmd = merged_mapping_dict.drop(columns=["value_mapping"]).rename(
+            columns={"combined_choices": "value_mapping"}
+        )
 
-        self.mapped_values = pd.Series(value_mapping, name="value_mapping")
-        self.mapped_values.index.name = self.INDEX_FIELD
-
-        return self.mapped_values
+        return mmd
 
     def post_process_mapping(self, mapping_dict, save, file_name) -> pd.DataFrame:
         """
-        Turn lists & dicts into strings for consistancy with saved CSV, then save.
+        Turn lists & dicts into strings for consistency with saved CSV, then save.
         """
+
+        if "choices" in self.data_dictionary:
+            mapping_dict = self._relabel_choices(mapping_dict)
+
         for col in ["common_values", "target_values"]:
             if col in mapping_dict.columns:
                 mapping_dict[col] = mapping_dict[col].apply(
@@ -208,6 +233,31 @@ class BaseMapper(abc.ABC):
                 file_name += ".csv"
             mapping_dict.to_csv(file_name)
             return mapping_dict
+
+    def match_values_to_schema(self) -> pd.DataFrame:
+        """
+        Use the LLM to match the common values from the data dictionary to the target
+        values in the schema - i.e. enum or boolean options.
+        """
+
+        values_tuples = list(self._iter_value_tuples())
+
+        # to LLM
+        value_pairs = self.model.map_values(values_tuples, self.language)
+
+        value_mapping = {}
+
+        for p in value_pairs.values:
+            f = p.field_name
+            value_dict = {
+                pair.source_value: pair.target_value for pair in p.mapped_values
+            }
+            value_mapping[f] = value_dict
+
+        self.mapped_values = pd.Series(value_mapping, name="value_mapping")
+        self.mapped_values.index.name = self.INDEX_FIELD
+
+        return self.mapped_values
 
 
 class WideMapper(BaseMapper):
@@ -257,48 +307,6 @@ class WideMapper(BaseMapper):
             t = self.target_values[f]
             if s is not None and t is not None:
                 yield (f, s, t)
-
-    def _relabel_choices(self, map_df):
-        """
-        If 'choices' are present in the data dictionary, relabel the choices
-        in the mapping dataframe to match the source field names.
-        e.g.
-        if the data dictionary indicates {1: Man, 2:Female, 3:Unknown}, and the LLM maps
-        {Man:male, Female:female, Unknown:unknown}, then `relabel_choices` should give
-        {1:male, 2:female, 3:unknown}
-        """
-
-        filtered_dict = self.data_dictionary[
-            self.data_dictionary["source_field"].isin(map_df["source_field"])
-        ][["source_field", "choices"]]
-
-        mapping_choices = map_df[["source_field", "value_mapping"]]
-
-        choices = filtered_dict.merge(mapping_choices, on="source_field")
-        choices["combined_choices"] = choices.apply(
-            lambda x: (
-                {k: x["value_mapping"].get(v) for k, v in x["choices"].items()}
-                if isinstance(x["choices"], dict)
-                else None
-            ),
-            axis=1,
-        )
-
-        merged_mapping_dict = (
-            map_df.reset_index()
-            .merge(
-                choices[["source_field", "combined_choices"]],
-                on="source_field",
-                how="left",
-            )
-            .drop_duplicates(subset="target_field")
-            .set_index("target_field")
-        )
-        mmd = merged_mapping_dict.drop(columns=["value_mapping"]).rename(
-            columns={"combined_choices": "value_mapping"}
-        )
-
-        return mmd
 
     def match_fields_to_schema(self) -> pd.DataFrame:
         """
@@ -394,9 +402,6 @@ class WideMapper(BaseMapper):
                 f"The following schema fields have not been mapped: {list(unmapped)}",
                 UserWarning,
             )
-
-        if "choices" in self.data_dictionary:
-            mapping_dict = self._relabel_choices(mapping_dict)
 
         return self.post_process_mapping(mapping_dict, save=save, file_name=file_name)
 
