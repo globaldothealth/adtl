a34b435128b2e3e1500a35c00d9297416acfe2d7
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2025-10-21 15:23:04 +0100

    Move some single use data into relevent function

--- tests/test_adtl/test_one_to_many_parser.py
+++ tests/test_adtl/test_one_to_many_parser.py
@@ -42,65 +42,6 @@ def test_one_to_many_with_common_mappings():
     assert one_many_output_rows == ONE_MANY_OUTPUT_COMMON
 
 
-ONE_MANY_IF_OUTPUT = [
-    {
-        "date": "2022-02-05",
-        "name": "headache",
-        "phase": "admission",
-        "is_present": False,
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-05",
-        "name": "oxygen_saturation",
-        "phase": "admission",
-        "value": 87.0,
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-05",
-        "name": "cough",
-        "phase": "admission",
-        "is_present": True,
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-05",
-        "name": "pao2_sample_type",
-        "phase": "study",
-        "text": "Capillary",
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-06",
-        "name": "history_of_fever",
-        "phase": "followup",
-        "is_present": True,
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-07",
-        "name": "history_of_fever",
-        "phase": "followup",
-        "is_present": False,
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-05",
-        "name": "fatigue_malaise",
-        "phase": "followup",
-        "is_present": True,
-        "adtl_valid": True,
-    },
-    {
-        "date": "2022-02-05",
-        "name": "severe_dehydration",
-        "phase": "admission",
-        "is_present": False,
-        "adtl_valid": True,
-    },
-]
-
 ONE_MANY_IF_MISSINGDATA_OUTPUT = [
     {
         "date": "2022-02-05",
@@ -131,47 +72,102 @@ def test_one_to_many_correct_if_behaviour():
         .read_table("observation")
     )
 
-    assert actual_row == ONE_MANY_IF_OUTPUT
+    assert actual_row == [
+        {
+            "date": "2022-02-05",
+            "name": "headache",
+            "phase": "admission",
+            "is_present": False,
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-05",
+            "name": "oxygen_saturation",
+            "phase": "admission",
+            "value": 87.0,
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-05",
+            "name": "cough",
+            "phase": "admission",
+            "is_present": True,
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-05",
+            "name": "pao2_sample_type",
+            "phase": "study",
+            "text": "Capillary",
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-06",
+            "name": "history_of_fever",
+            "phase": "followup",
+            "is_present": True,
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-07",
+            "name": "history_of_fever",
+            "phase": "followup",
+            "is_present": False,
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-05",
+            "name": "fatigue_malaise",
+            "phase": "followup",
+            "is_present": True,
+            "adtl_valid": True,
+        },
+        {
+            "date": "2022-02-05",
+            "name": "severe_dehydration",
+            "phase": "admission",
+            "is_present": False,
+            "adtl_valid": True,
+        },
+    ]
     assert actual_row_missing == ONE_MANY_IF_MISSINGDATA_OUTPUT
-
-
-APPLY_OBSERVATIONS_SOURCE = [
-    {
-        "dsstdat": "2023-02-01",
-        "flw_headache": "1",
-        "flw_cough": "1",
-        "dyspnea_cmyn": "0",
-    }
-]
-
-APPLY_OBSERVATIONS_OUTPUT = [
-    {
-        "date": "2023-02-01",
-        "start_date": "2023-01-22",
-        "phase": "followup",
-        "duration_type": "event",
-        "name": "headache",
-        "is_present": True,
-    },
-    {
-        "date": "2023-02-01",
-        "start_date": "2023-01-25",
-        "phase": "followup",
-        "duration_type": "event",
-        "name": "cough",
-        "is_present": True,
-    },
-]
 
 
 def test_apply_in_one_to_many():
     apply_observations_output = list(
         parser.Parser(parser_path / "apply-observations.toml")
-        .parse_rows(APPLY_OBSERVATIONS_SOURCE, "apply_obs")
+        .parse_rows(
+            [
+                {
+                    "dsstdat": "2023-02-01",
+                    "flw_headache": "1",
+                    "flw_cough": "1",
+                    "dyspnea_cmyn": "0",
+                }
+            ],
+            "apply_obs",
+        )
         .read_table("observation")
     )
 
-    assert apply_observations_output == APPLY_OBSERVATIONS_OUTPUT
+    assert apply_observations_output == [
+        {
+            "date": "2023-02-01",
+            "start_date": "2023-01-22",
+            "phase": "followup",
+            "duration_type": "event",
+            "name": "headache",
+            "is_present": True,
+        },
+        {
+            "date": "2023-02-01",
+            "start_date": "2023-01-25",
+            "phase": "followup",
+            "duration_type": "event",
+            "name": "cough",
+            "is_present": True,
+        },
+    ]
 
 
 # Default 'if' rule data & test
