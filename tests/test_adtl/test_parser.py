75929fa6ead16a1ad414284cd05457c3a01c7c4c
Pip Liggins <philippa.liggins@dtc.ox.ac.uk>
2025-10-21 12:16:06 +0100

    Move 'main' tests to their own file

--- tests/test_adtl/test_parser.py
+++ tests/test_adtl/test_parser.py
@@ -6,27 +6,15 @@ import io
 import json
 import warnings
 from pathlib import Path
-from typing import Any, Dict, Iterable
 
 import pytest
-import responses
 from pytest_unordered import unordered
 
-import adtl
 import adtl.parser as parser
 
 TEST_PARSERS_PATH = Path(__file__).parent / "parsers"
 TEST_SOURCES_PATH = Path(__file__).parent / "sources"
 TEST_SCHEMAS_PATH = Path(__file__).parent / "schemas"
-
-ARGV = [
-    str(TEST_PARSERS_PATH / "epoch.json"),
-    str(TEST_SOURCES_PATH / "epoch.csv"),
-    "-o",
-    "output",
-    "--encoding",
-    "utf-8",
-]
 
 LIVER_DISEASE = [
     {
@@ -323,10 +311,6 @@ OBSERVATION_RULE_FIELD_OPTION_VALUE_COMB = {
         ],
     },
 }
-
-
-def _subdict(d: Dict, keys: Iterable[Any]) -> Dict[str, Any]:
-    return {k: d.get(k) for k in keys}
 
 
 def test_one_to_many():
@@ -904,82 +888,6 @@ def test_skip_field_pattern_absent(snapshot):
         .write_csv("table")
     )
     assert transformed_csv_data == snapshot
-
-
-def test_main(snapshot):
-    adtl.main(ARGV)
-    assert Path("output-table.csv").read_text() == snapshot
-    Path("output-table.csv").unlink()
-
-
-def test_main_parquet():
-    adtl.main(ARGV + ["--parquet"])
-    assert Path("output-table.parquet")
-    Path("output-table.parquet").unlink()
-
-
-def test_main_parquet_error():
-    ARG = [
-        str(TEST_PARSERS_PATH / "return-unmapped.toml"),
-        str(TEST_SOURCES_PATH / "return-unmapped.csv"),
-        "-o",
-        "output",
-        "--encoding",
-        "utf-8",
-    ]
-
-    with pytest.raises(
-        ValueError, match="returnUnmatched and parquet options are incompatible"
-    ):
-        adtl.main(ARG + ["--parquet"])
-
-
-@responses.activate
-def test_main_web_schema(snapshot):
-    # test with schema on the web
-    epoch_schema = json.loads(
-        Path(TEST_SCHEMAS_PATH / "epoch-data.schema.json").read_text()
-    )
-    responses.add(
-        responses.GET,
-        "http://example.com/schemas/epoch-data.schema.json",
-        json=epoch_schema,
-        status=200,
-    )
-    adtl.main([str(TEST_PARSERS_PATH / "epoch-web-schema.json")] + ARGV[1:])
-    assert Path("output-table.csv").read_text() == snapshot
-    Path("output-table.csv").unlink()
-
-
-@responses.activate
-def test_main_web_schema_missing(snapshot):
-    responses.add(
-        responses.GET,
-        "http://example.com/schemas/epoch-data.schema.json",
-        json={"error": "not found"},
-        status=404,
-    )
-    adtl.main([str(TEST_PARSERS_PATH / "epoch-web-schema.json")] + ARGV[1:])
-    assert Path("output-table.csv").read_text() == snapshot
-    Path("output-table.csv").unlink()
-
-
-def test_main_save_report():
-    adtl.main(ARGV + ["--save-report", "epoch-report.json"])
-    report = json.loads(Path("epoch-report.json").read_text())
-    assert report["file"].endswith("tests/test_adtl/sources/epoch.csv")
-    assert report["parser"].endswith("tests/test_adtl/parsers/epoch.json")
-    assert _subdict(
-        report,
-        ["encoding", "include_defs", "total", "total_valid", "validation_errors"],
-    ) == {
-        "encoding": "utf-8",
-        "include_defs": [],
-        "total": {"table": 2},
-        "total_valid": {"table": 2},
-        "validation_errors": {},
-    }
-    Path("epoch-report.json").unlink()
 
 
 OVERWRITE_OUTPUT = [
